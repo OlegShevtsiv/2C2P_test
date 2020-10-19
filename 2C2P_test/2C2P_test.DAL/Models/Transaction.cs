e61be5f75f24c1da8b59a1ad439296eef24a1657
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2020-10-19 21:05:21 +0300

    Add Data Acces Layer
    Created Transaction model, Repository and UnitOfWork interfaces.

---
+++ 2C2P_test/2C2P_test.DAL/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2C2P_test.DAL.Models
+{
+    public sealed class Transaction
+    {
+        public string Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string CurrencyCode { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public object Status { get; set; }
+
+        public Transaction()
+        {
+
+        }
+    }
+}
---
+++ 2C2P_test/2C2P_test.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using _2C2P_test.DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2C2P_test.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>()
+            where TEntity : class;
+
+        int SaveChanges();
+    }
+}

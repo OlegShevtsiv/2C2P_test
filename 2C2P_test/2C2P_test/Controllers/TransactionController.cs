5275b39dce116c0666e1bd1ef5dab52f0ea97bbb
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2020-10-19 20:21:01 +0300

    Create ASP .Net Core Web API project

---
+++ 2C2P_test/2C2P_test/Controllers/TransactionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _2C2P_test.Controllers
+{
+    [Route("api/transaction")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Hello() 
+        {
+            return Ok("Hello!");
+        }
+    }
+}
